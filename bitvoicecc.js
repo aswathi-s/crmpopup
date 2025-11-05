ff5cd14733840d60195fee2fa04fd04bc8deb50f
Aswathi <164324449+aswathi-s@users.noreply.github.com>
2025-11-05 15:37:57 +0530

    1Update bitvoicecc.js

--- bitvoicecc.js
+++ bitvoicecc.js
@@ -628,9 +628,9 @@ async function processIncomingEvent(eventData) {
 
         if (contacts.list.length > 0) {
             contactId = contacts.list[0].id;
-            // 3. Search linked Opportunities
+            // 3. Search linked Account
             const linkedAcct = await Espo.Ajax.getRequest(
-                `Account?maxSize=1&where[0][attribute]=contactId&where[0][type]=equals&where[0][value]=${contactId}`
+                `Account?maxSize=1&where[0][attribute]=phoneNumber&where[0][type]=equals&where[0][value]=${encodePhoneNumberForApi(phoneNumber)}`
             );
             if (linkedAcct.list.length > 0) {
                 linkedAccId = linkedAcct.list[0].id;
